b2c086693c3eabefb74e264428646c53f9d1a05d
madhabilatabarua313-hub <madhabilatabarua313@gmail.com>
2026-09-21 19:57:29 +0600

    Update app.py

--- app.py
+++ app.py
@@ -243,13 +243,14 @@ ex_pdf = generate_pdf_report(
     summary_items=ex_summary_items,
 )
 
-     # PDF Download Button
-     st.download_button(
+# PDF Download Button
+    st.download_button(
         label="Download Section Report (.pdf)",
         data=ex_pdf,
         file_name="Excavation_Report.pdf",
         mime="application/pdf"
     )
+
 # 3. ADVANCED CONCRETE VOLUME (Beam, Column, Slab, Stair)
 # --------------------------------------------------
 elif st.session_state["calc_type"] == "🧱 Concrete Volume (Beam, Column, Slab, Stair)":
